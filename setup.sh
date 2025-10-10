d46e672ba850633390690d1b1b24847c724e57d1
vb52ru <vadim52rus@gmail.com>
2025-10-10 14:18:45 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/zsh
 wget https://raw.githubusercontent.com/vb52ru/notes/refs/heads/main/.zshrc -O ~/.zshrc
 wget https://raw.githubusercontent.com/vb52ru/notes/refs/heads/main/.vimrc -O ~/.vimrc
 source ~/.zshrc
