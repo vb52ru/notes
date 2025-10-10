35ff6072a1f5a716ff9bb4e780ffca5fe94cb2fb
vb52ru <vadim52rus@gmail.com>
2025-10-10 14:22:49 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/zsh
+apt install bat git -y
 wget https://raw.githubusercontent.com/vb52ru/notes/refs/heads/main/.zshrc -O ~/.zshrc
 wget https://raw.githubusercontent.com/vb52ru/notes/refs/heads/main/.vimrc -O ~/.vimrc
+git config --global pager.log false
 source ~/.zshrc
