f8038c5fa62ec789fe954b937972c3956b239a02
vb52ru <vadim52rus@gmail.com>
2025-10-10 14:06:11 +0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+wget https://raw.githubusercontent.com/vb52ru/notes/refs/heads/main/.zshrc -O ~/.zshrc
+wget https://raw.githubusercontent.com/vb52ru/notes/refs/heads/main/.vimrc -O ~/.vimrc
+source ~/.zshrc
